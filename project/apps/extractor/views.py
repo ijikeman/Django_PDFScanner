87644bb091ba4ca0a11504784a97204c82af5cca
ijikeman <keiji.ue@gmail.com>
2025-02-16 17:39:01 +0900

    fixed

--- project/apps/extractor/views.py
+++ project/apps/extractor/views.py
@@ -1,27 +1,25 @@
-#from django.shortcuts import render
-
-# Create your views here.
 import os
 from django.shortcuts import render, redirect
 from .forms import PDFUploadForm
 from .models import PDFDocument
 import PyPDF2
-
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    with open(pdf_path, "rb") as f:
-        reader = PyPDF2.PdfReader(f)
-        for page in reader.pages:
-            text += page.extract_text() or ""
-    return text
+from modules import PdfReader
 
 def upload_pdf(request):
     if request.method == 'POST':
         form = PDFUploadForm(request.POST, request.FILES)
+
         if form.is_valid():
             pdf_instance = form.save()
-            text = extract_text_from_pdf(pdf_instance.file.path)
+            pdf_path = pdf_instance.file.path
+            page_numbers = form.cleaned_data.get('page_numbers', [])  # 取得
+            print("Extracted page_numbers:", page_numbers)  # デバッグ用
+            reader = PdfReader(pdf_instance.file.path, page_numbers) # PdfReaderを使うように改変
+            text = reader.pdfplumber()
             return render(request, 'extractor/result.html', {'text': text})
+        else:
+            print("Form errors:", form.errors)  # ここでエラー内容を確認！
     else:
         form = PDFUploadForm()
+
     return render(request, 'extractor/upload.html', {'form': form})
