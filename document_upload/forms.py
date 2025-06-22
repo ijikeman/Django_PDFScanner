89616d89fdefd9197a6b6c7b99c9f3052b024343
ijikeman <keiji.ue@gmail.com>
2025-06-22 23:14:30 +0900

    fixed template

--- document_upload/forms.py
+++ document_upload/forms.py
@@ -5,3 +5,7 @@ class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ['name', 'file']
+        labels = {
+            'name': 'ファイル名',
+            'file': 'ファイル',
+        }
